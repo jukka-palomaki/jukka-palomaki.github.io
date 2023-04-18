fbfe3b8a39f946130cd52120d4d6a8bc2fdb8c33
PALOMÄKI Jukka <jukka.palomaki@alten.fi>
2023-04-18 15:16:04 +0300

    Register service worker

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log('Hello from script.js!');
+
+/* Only register a service worker if it's supported */
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/service-worker.js");
+}
+  
